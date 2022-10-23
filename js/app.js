5a92fd91c2712149f5e80ace339fa1beb8762d65
Orzxd <98451082+Orzxd@users.noreply.github.com>
2022-10-24 00:49:40 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
-console.log('I am running!');
+let Num_visited =  document.getElementById('num_visted');
+let Num_A0 =  document.getElementById('num_A0');
+let Num_A3 =  document.getElementById('num_A3');
+let Num_A4 =  document.getElementById('num_A4');
+let cost_visited =  document.getElementById('cost_visted');
+let cost_A0 =  document.getElementById('cost_A0');
+let cost_A3 =  document.getElementById('cost_A3');
+let cost_A4 =  document.getElementById('cost_A4');
+
+function Calc(){
+    if (Num_visited.value.length === 0) {
+        return;
+    }
+    if (Num_A0.value.length === 0) {
+        return;
+    }
+    if (Num_A3.value.length === 0) {
+        return;
+    }
+    if (Num_A4.value.length === 0) {
+        return;
+    }
+
+    cost_visited.value = Num_visited.value * 7.3;
+    cost_A0.value = Num_A0.value * 76;
+    cost_A3.value = Num_A3.value * 1.5;
+    cost_A4.value = Num_A4.value * 1.3;
+}
+
+
+
+// Register service worker to control making site work offline
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(() => { console.log('Service Worker Registered'); });
+}
